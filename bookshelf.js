ba27409b50e6dce64eb96fc5bdf7e9469cf5f372
ceciliaknaub <ccknaub@gmail.com>
2023-12-07 08:48:12 -0500

    Revert "books as 2d shapes"
    This reverts commit 8cefe11e52af653a5df4f1c6ddfeaef7b2f81cdc.

--- bookshelf.js
+++ bookshelf.js
@@ -14,74 +14,8 @@ let book2;
 let font1;
 let font2;
 
-function preload(){
-  book1 = loadImage('./assets/to_the_lighthouse_cover.png');
-  book2 =  loadImage('./assets/shoe_dog_cover.jpg');
-  book3 = loadImage('./assets/didion_cover.png');
-  book4 = loadImage('./assets/bell_hooks_cover.png');
-  font1 = loadFont('./assets/Junge/Junge-Regular.ttf');
-  font2 = loadFont('./assets/Hind/Hind-Bold.ttf');
-}
-
-let objects = []
-let dRay;
-let ray;
-
-
-function slct(h,w,d,x, y, z, img,author,title,font,fontColor)  {
-  return(
-    {
-      h: h,
-      w: w,
-      d: d,
-      x: x,
-      y: y,
-      z: z,
-      image: img,
-      author: author,
-      title: title,
-      font: font,
-      fontColor: fontColor
-    }
-  )
-}
-
-function setup() {
-  canvas = createCanvas(windowWidth, windowHeight, WEBGL);
-  
-  // Add shapes to objects array
-  objects.push(slct(500, 50, 0, 0, 0, 0, book1,'Virginia Woolf','To The Lighthouse',font1,'white'))
-  objects.push(slct(475, 65, 0, 58, 12.50, 0, book2,'Phil Knight','Shoe Dog',font2,'#A9672A'))
-  objects.push(slct(480, 45, 0, 114, 10, 0,book3,'Joan Didion','Title',font1,'black'))
-  objects.push(slct(500, 50, 0, 161, 0 , 0,book4,'bell hooks','all about love',font1,'black'))
-
-  background(roomColor);
-  //background('white');
-  canvas.position(w/12,0);
-}
-
-function draw() {   
-  noStroke();
-
-  for(let i = 0; i < objects.length; i++){
- drawBook(objects[i].w,objects[i].h,objects[i].d,objects[i].x,objects[i].y,objects[i].z,objects[i].author,objects[i].title,objects[i].font,objects[i].fontColor,objects[i].image);
-  }
-}
-
-function drawBook(x,y,z,objX,objY,objZ,author,title,font,fontColor,img) {
-    push()
-      translate(objX,objY,objZ)
-      texture(img);
-      box(x,y,z)
-    pop();
-
-    push()
-    translate(objX,objY,5)
-    bookText(author,title,font,fontColor);
-    pop();
-}
-
 document.addEventListener('DOMContentLoaded', function () {
+
 
     const date = new Date();
     let hour = date.getHours();
@@ -95,18 +29,59 @@ document.addEventListener('DOMContentLoaded', function () {
           else {
             roomColor = roomBackgroundColors.morning;
           }
+
+
     document.querySelector('.bookshelf-container').style.background = roomColor;
-    
+
 });
 
-function bookText(author,title,font,fontColor) {
+function preload(){
+  book1 = loadImage('./assets/to_the_lighthouse_cover.png');
+  book2 =  loadImage('./assets/shoe_dog_cover.jpg');
+  book3 = loadImage('./assets/didion_cover.png');
+  book4 = loadImage('./assets/bell_hooks_cover.png');
+  font1 = loadFont('./assets/Junge/Junge-Regular.ttf');
+  font2 = loadFont('./assets/Hind/Hind-Bold.ttf');
+}
+
+function setup() {
+  canvas = createCanvas(w/2,h,WEBGL);
+  background(roomColor);
+  //background('white');
+  canvas.position(w/4,0);
+}
+
+function draw(){
+  drawBook(500,50,0,book1,'Virginia Wolf','To The Lighthouse',font1,255);
+  
+  translate(57,12,0);
+  drawBook(475,65,0,book2,'Phil Knight','SHOE DOG',font2,'#A9672A');
+
+  translate(50,0,0);
+  drawBook(475,45,0,book3,'Joan Didion','The Year of Magical Thinking',font1,'black');
+
+  translate(50,0,0);
+  drawBook(475,45,0,book4,'bell hooks','all about love',font1,'black');
+}
+
+function drawBook(h,w,r,image, author, title,font,textColor) {
+  noStroke();
+  rotateZ(-PI*r/24);
+  texture(image);
+  box(w, h, 70);
+  bookText(author,title,font,textColor);
+}
+
+function bookText(author,title,font,textColor) {
+  push();
   textFont(font);
-  //translate(0,0,5);
+  fill(textColor);
+  translate(0,0,40);
   rotate(PI/2);
   textAlign(CENTER);
   textSize(14);
-  fill(fontColor);
   text(title,0, 5);
   textSize(18);
   text(author,-150, 5);
+  pop();
 }
